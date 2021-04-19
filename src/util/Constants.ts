a6e315efa5e5f0a06ffa7dbc761af606fdc40694
Lavish6 <73431292+Lavish6@users.noreply.github.com>
2021-04-19 22:12:25 +0200

    fix: interaction response types
    Updated types and removed deprecated types.

--- src/util/Constants.ts
+++ src/util/Constants.ts
@@ -29,8 +29,6 @@ export enum InteractionType {
  */
 export enum InteractionResponseType {
     PONG = 1,
-    ACKNOWLEDGE = 2,
-    CHANNEL_MESSAGE = 3,
     CHANNEL_MESSAGE_WITH_SOURCE = 4,
     DEFERRED_CHANNEL_MESSAGE_WITH_SOURCE = 5
 }
